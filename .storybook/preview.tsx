f187c425390d05e347965080a663d5dd1c84df21
Mitch Ryan <mitch@mitch-ryan.com>
2026-02-24 13:25:55 +0100

    Remove some Storybook config boilerplate

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -9,32 +9,6 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
-    backgrounds: {
-      options: {
-        "red-bull-dark": {
-          name: "red-bull-dark",
-          value: "#000F1ECC",
-        },
-
-        "callsessions-gradient-red": {
-          name: "callsessions-gradient-red",
-          value:
-            "linear-gradient(308deg, rgba(0, 0, 0, 0.00) 7.11%, rgba(221, 23, 44, 0.40) 67.08%), #001C39",
-        },
-
-        "callsessions-gradient-blue": {
-          name: "callsessions-gradient-blue",
-          value:
-            "linear-gradient(308deg, rgba(0, 0, 0, 0.00) 7.11%, rgba(116, 163, 241, 0.40) 67.08%), #0054AC;",
-        },
-      },
-    },
-  },
-
-  initialGlobals: {
-    backgrounds: {
-      value: "callsessions-gradient-blue",
-    },
   },
 };
 
