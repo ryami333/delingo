6afbf771a4601aa8a1877d1b566e60f1aad788af
Mitch Ryan <mitch@mitch-ryan.com>
2026-02-24 14:28:03 +0100

    Add pluralNoun and pluralEnglish columns to nouns database

--- src/helpers/nounSchema.ts
+++ src/helpers/nounSchema.ts
@@ -2,8 +2,10 @@ import z from "zod";
 
 export const nounSchema = z.object({
   noun: z.string(),
+  pluralNoun: z.string(),
   gender: z.enum(["f", "m", "n"]),
   english: z.string(),
+  pluralEnglish: z.string(),
 });
 
 export type Noun = z.infer<typeof nounSchema>;
