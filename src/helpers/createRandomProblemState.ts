5298d2014da3bb62475da01636de5542694dfc5f
Mitch Ryan <mitch@mitch-ryan.com>
2026-02-25 19:14:17 +0100

    Refactor to include problem+solution in problem state

--- src/helpers/createRandomProblemState.ts
+++ src/helpers/createRandomProblemState.ts
@@ -1,14 +1,33 @@
+import { artikelSchema } from "./artikelSchema";
+import artikels from "./artikels.json";
 import { getRandomIndex } from "./getRandomIndex";
 import { nounSchema } from "./nounSchema";
 import nouns from "./nouns.json";
+import capitalize from "lodash/capitalize";
 
 export const createRandomProblemState = () => {
-  const randomIndex = getRandomIndex(nouns);
-  const noun = nounSchema.parse(nouns.at(randomIndex));
+  const randomNounIndex = getRandomIndex(nouns);
+  const noun = nounSchema.parse(nouns.at(randomNounIndex));
+
+  const randomArtikelIndex = getRandomIndex(artikels);
+  const artikel = artikelSchema.parse(artikels.at(randomArtikelIndex));
+
   const plural = Math.random() > 0.75; // One-quarter of the time
+
+  const artikelProblem = artikel.english;
+  const nounProblem = plural ? noun.pluralEnglish : noun.english;
+  const problem = capitalize(`${artikelProblem} ${nounProblem}`);
+
+  const artikelSolution = artikel.nominativ[plural ? "pl" : noun.gender];
+  const nounSolution = capitalize(plural ? noun.pluralNoun : noun.noun);
+  const solution = `${artikelSolution} ${nounSolution}`;
+
   return {
     uuid: window.crypto.randomUUID(),
+    artikel,
     noun,
     plural,
+    problem,
+    solution,
   };
 };
