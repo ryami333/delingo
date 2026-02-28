556e4c366b57496bcb7919938e7899b1f3bba4a9
Mitch Ryan <mitch@diesdas.digital>
2026-02-28 12:38:19 +0100

    Update ProblemPart generic to only accept known entities

--- src/helpers/problems/AbstractProblem.ts
+++ src/helpers/problems/AbstractProblem.ts
@@ -1,4 +1,10 @@
-export type ProblemPart<T = unknown> = readonly [string, T];
+import { Artikel } from "../artikelSchema";
+import { Noun } from "../nounSchema";
+import { Pronoun } from "../pronounSchema";
+import { Verb } from "../verbSchema";
+
+export type Entity = Noun | Artikel | Pronoun | Verb;
+export type ProblemPart<T extends Entity = Entity> = readonly [string, T];
 
 export abstract class AbstractProblem {
   public uuid: string;
