f6a8822faa11e1bddb817260d254d62a2f125b85
Mitch Ryan <mitch@mitch-ryan.com>
2026-02-28 11:07:29 +0100

    Remove unused modules+helpers from template project

--- src/helpers/cloneImageData.ts
+++
@@ -1,7 +0,0 @@
-export const cloneImageData = (source: ImageData) => {
-  const newImageData = new ImageData(source.width, source.height, {
-    colorSpace: source.colorSpace,
-  });
-  newImageData.data.set(source.data);
-  return newImageData;
-};
--- src/helpers/db.ts
+++
@@ -1,4 +0,0 @@
-import { env } from "./env.mjs";
-import { MongoClient } from "mongodb";
-
-export const db = new MongoClient(env.MONGODB_URL).db("mitchs-record-club");
