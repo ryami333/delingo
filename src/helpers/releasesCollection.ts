630de2448b6f74ec7937060bbcaaaeccc48f093d
Mitch Ryan <mitch@diesdas.digital>
2026-02-28 11:07:29 +0100

    Remove unused modules+helpers from template project

--- src/helpers/releasesCollection.ts
+++
@@ -1,6 +0,0 @@
-import { db } from "./db";
-import { releaseSchema } from "./releaseSchema";
-import z from "zod";
-
-export const releasesCollection =
-  db.collection<z.infer<typeof releaseSchema>>("releases");
