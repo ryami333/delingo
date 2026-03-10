115db6526d6349644656f0782015102d599c1acf
Mitch Ryan <mitch@diesdas.digital>
2026-03-10 20:46:16 +0100

    Update PartsAccordion to show helpful tips for each entity type

--- src/components/PartsAccordion.tsx
+++ src/components/PartsAccordion.tsx
@@ -7,7 +7,39 @@ export function PartsAccordion({ parts }: { parts: [string, Entity][] }) {
       {parts.map(([word, entity], index) => (
         <Accordion.Item key={index} value={index.toString()}>
           <Accordion.Control>{word}</Accordion.Control>
-          <Accordion.Panel>{JSON.stringify(entity)}</Accordion.Panel>
+          <Accordion.Panel>
+            {(() => {
+              switch (entity.__type) {
+                case "pronoun": {
+                  return `${entity.pronoun}`;
+                }
+                case "wechselpreposition": {
+                  return `${entity.preposition}`;
+                }
+                case "preposition": {
+                  return `${entity.preposition} • ${entity.form}`;
+                }
+                case "artikel": {
+                  return `${entity.word}`;
+                }
+                case "intransitiveVerb": {
+                  return `${entity.verb} • ${entity.direction}`;
+                }
+                case "transitiveVerb": {
+                  return `${entity.verb} • ${entity.form}`;
+                }
+                case "noun": {
+                  return `${entity.nominativ.singular} • ${entity.gender}`;
+                }
+                default: {
+                  // @ts-expect-error: switch should be exhaustive.
+                  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+                  entity.__type;
+                  throw new Error("Unhandled case");
+                }
+              }
+            })()}
+          </Accordion.Panel>
         </Accordion.Item>
       ))}
     </Accordion>
