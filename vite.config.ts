8f3ec12ae83a812d2364e41d612c965207c0a7af
Mitch Ryan <mitch@diesdas.digital>
2026-03-30 21:50:01 +0200

    Bump to Vite 8, create dedicated vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,26 @@
+import babel from "@rolldown/plugin-babel";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact from "@vitejs/plugin-react";
+import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => ({
-  plugins:
-    mode === "test" ? [viteReact()] : [nitro(), tanstackStart(), viteReact()],
-}));
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  css: {
+    modules: {
+      generateScopedName: "[name]__[local]__[hash:base64:5]",
+    },
+  },
+  plugins: [
+    tanstackStart({ srcDirectory: "src" }),
+    viteReact(),
+    babel({
+      presets: [reactCompilerPreset()],
+    }),
+    nitro({
+      noExternals: ["graphql"],
+    }),
+  ],
+});
