bbceecbeb3538a4309c34ebf100cf5456726277c
Mitch Ryan <mitch@mitch-ryan.com>
2026-06-16 20:46:52 +0200

    Provide template for CheatSheet

---
+++ src/components/CheatSheet.tsx
@@ -0,0 +1,23 @@
+export function CheatSheet() {
+  return (
+    <table>
+      <tr>
+        <td rowSpan={4}>Nominativ</td>
+        <td>m</td>
+        <td>kein neuer Film</td>
+      </tr>
+      <tr>
+        <td>n</td>
+        <td>kein neues Buch</td>
+      </tr>
+      <tr>
+        <td>f</td>
+        <td>keine neue Serie</td>
+      </tr>
+      <tr>
+        <td>pl</td>
+        <td>keine neuen Filme</td>
+      </tr>
+    </table>
+  );
+}
