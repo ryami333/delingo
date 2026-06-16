03fe5687675a7ef7eb5bcc76a960ce14bbc77138
Mitch Ryan <mitch@mitch-ryan.com>
2026-06-16 20:51:00 +0200

    Complete all the cases in the declension Cheatsheet

--- src/components/CheatSheet.tsx
+++ src/components/CheatSheet.tsx
@@ -18,6 +18,57 @@ export function CheatSheet() {
         <td>pl</td>
         <td>keine neuen Filme</td>
       </tr>
+      <tr>
+        <td rowSpan={4}>Akkusativ</td>
+        <td>m</td>
+        <td>keinen neuen Film</td>
+      </tr>
+      <tr>
+        <td>n</td>
+        <td>kein neues Buch</td>
+      </tr>
+      <tr>
+        <td>f</td>
+        <td>keine neue Serie</td>
+      </tr>
+      <tr>
+        <td>pl</td>
+        <td>keine neuen Filme</td>
+      </tr>
+      <tr>
+        <td rowSpan={4}>Dativ</td>
+        <td>m</td>
+        <td>keinem neuen Film</td>
+      </tr>
+      <tr>
+        <td>n</td>
+        <td>keinem neuen Buch</td>
+      </tr>
+      <tr>
+        <td>f</td>
+        <td>keiner neuen Serie</td>
+      </tr>
+      <tr>
+        <td>pl</td>
+        <td>keinen neuen Filmen</td>
+      </tr>
+      <tr>
+        <td rowSpan={4}>Genitiv</td>
+        <td>m</td>
+        <td>keines neuen Films</td>
+      </tr>
+      <tr>
+        <td>n</td>
+        <td>keines neuen Buches</td>
+      </tr>
+      <tr>
+        <td>f</td>
+        <td>keiner neuen Serie</td>
+      </tr>
+      <tr>
+        <td>pl</td>
+        <td>keiner neuen Filme</td>
+      </tr>
     </table>
   );
 }
