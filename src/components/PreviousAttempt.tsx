3b51e796bfcaec5dc42a5005a9d33904b29ff781
Mitch Ryan <mitch@mitch-ryan.com>
2026-06-17 09:32:20 +0200

    Add Blockquote to PreviousAttempt component

--- src/components/PreviousAttempt.tsx
+++ src/components/PreviousAttempt.tsx
@@ -1,5 +1,5 @@
 import { Feedback, FeedbackKind } from "../helpers/getAttemptFeedback";
-import { Text } from "@mantine/core";
+import { Blockquote, Text } from "@mantine/core";
 
 function getFeedbackKindColor(kind: FeedbackKind): string {
   switch (kind) {
@@ -18,12 +18,14 @@ function getFeedbackKindColor(kind: FeedbackKind): string {
 
 export function PreviousAttempt({ feedback }: { feedback: Feedback[] }) {
   return (
-    <Text>
-      {feedback.map((item, index) => (
-        <span key={index} style={{ color: getFeedbackKindColor(item.kind) }}>
-          {item.text}
-        </span>
-      ))}
-    </Text>
+    <Blockquote>
+      <Text>
+        {feedback.map((item, index) => (
+          <span key={index} style={{ color: getFeedbackKindColor(item.kind) }}>
+            {item.text}
+          </span>
+        ))}
+      </Text>
+    </Blockquote>
   );
 }
